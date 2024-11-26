5cfbab43ba78e73106033981075e02df7b5eda06
alif-ul-haque <alifulhaque19@gmail.com>
2024-11-26 23:20:18 +0600

    Add files via upload

---
+++ week05/Day03/A_We_Need_the_Zero.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define granite_state ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long
+#define line cout << "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define vsb (v).begin(),(v).end()
+#define vse (v).rbegin(),(v).rend()
+#define spc " "
+#define pb push_back
+#define pf push_front;
+
+signed main()
+ {
+    granite_state
+     int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        int cnt = -1;
+        for(int i=0;i<256;i++)
+        {
+            int s;
+            s = v[n-1]^i;
+            for(int j=n-2;j>=0;j--)
+            {
+                s = s^(v[j]^i);
+            }
+            if(s==0)
+            {
+                cnt=i;
+                break;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+}
+
+    ////////////////////////////////////////////////////
+    //                                                 //
+    //                 A                               //
+    //                                                 //
+    //                 L                               //
+    //                                                 //
+    //                 I                               //
+    //                                                 //
+    //                 F                               //
+    /////////////////////////////////////////////////////
+//think simply, it's always easier than it seems at first
+    //PEN AND PAPER instead of ms paint
+    //keep at it even if it feels exhausting(we're not doing this for fun anymore)
+    //2 GHONTAR AGER EDITORIAL DHORBI NA
---
+++ week05/Day03/C_Tricky_Template.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define granite_state ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long
+#define line cout << "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define vsb (v).begin(),(v).end()
+#define vse (v).rbegin(),(v).rend()
+#define spc " "
+#define pb push_back
+#define pf push_front;
+
+signed main()
+ {
+    granite_state
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string a,b,c;
+        cin>>a>>b>>c;
+        bool flag = false;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==b[i] && a[i]!=c[i] || a[i]!=b[i] && a[i]!=c[i] && b[i]!=c[i])
+           {
+              flag = true;
+              break;
+           }
+        }
+        if(flag) YES;
+        else NO;
+    }
+}
+
+    ////////////////////////////////////////////////////
+    //                                                 //
+    //                 A                               //
+    //                                                 //
+    //                 L                               //
+    //                                                 //
+    //                 I                               //
+    //                                                 //
+    //                 F                               //
+    /////////////////////////////////////////////////////
+//think simply, it's always easier than it seems at first
+    //PEN AND PAPER instead of ms paint
+    //keep at it even if it feels exhausting(we're not doing this for fun anymore)
+    //2 GHONTAR AGER EDITORIAL DHORBI NA
